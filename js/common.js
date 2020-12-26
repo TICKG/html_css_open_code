ccfd2a237a0b96217d92caa3a36932b0e28bd9e1
TAICHI　KAGA <42921170+TICKG@users.noreply.github.com>
2020-12-26 12:43:48 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,19 @@
+         $('.slick1').slick({
+           autoplay: true,
+           autoplaySpeed: 3000,
+           arrows: false,
+           slidesToShow: 7,
+           slidesToScroll: 1,
+         });
+
+         $('.slick2').slick({
+           centerMode: true,
+           centerPadding: '20px',
+           autoplay: false,
+           autoplaySpeed: 3000,
+           arrows: true,
+           slidesToShow: 7,
+           slidesToScroll: 1,
+           prevArrow: '<img class="prev" src="https://www.linguage-school.jp/wp-content/themes/linguage/img/lp/arrow-slide_l.png">',
+           nextArrow: '<img class="next" src="https://www.linguage-school.jp/wp-content/themes/linguage/img/lp/arrow-slide_r.png">'
+         });
